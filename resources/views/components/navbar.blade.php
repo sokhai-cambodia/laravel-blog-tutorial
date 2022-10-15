c89387354f3bbe6afa6db65c343f814f415d034b
hai <hai.sok@wingmoney.com>
2022-10-15 10:22:46 +0700

    add home & blog page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,9 +15,6 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-              <a class="nav-link" href="#">Main Menu</a>
-          </li>
-          <li class="nav-item">
             <a class="nav-link" href="#">Category 1</a>
           </li>
           <li class="nav-item">
